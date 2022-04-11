57633796474e90a03227e5472b7300ff72dad6da
majic <65995144+majiicmajic@users.noreply.github.com>
2022-04-11 13:45:51 +0100

    Add project files.

---
+++ MajicgRPCClient/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Net.Client;
+using MajicgRPCClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MajicgRPCClient.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public async Task<IActionResult> IndexAsync()
+		{
+			 // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "joseph" });
+
+			ResponseViewModel responseViewModel = new ResponseViewModel
+			{
+				HelloReply = reply
+			};
+      
+			return View(responseViewModel);
+		}
+
+
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ MajicgRPCClient/Models/ResponseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MajicgRPCClient.Models
+{
+	public class ResponseViewModel
+	{
+
+		public HelloReply HelloReply { get; set; }
+	}
+}
